04faa78393a78af385b84bee765f4a946b9153d4
YuPhe1 <oksunboy1@naver.com>
2023-10-12 11:24:49 +0900

    14.댓글 삭제

--- src/main/java/com/icia/memberboard/controlller/CommentController.java
+++ src/main/java/com/icia/memberboard/controlller/CommentController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,21 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/save")
+    @PostMapping
     public ResponseEntity save(@RequestBody CommentDTO commentDTO){
-        System.out.println("commentDTO = " + commentDTO);
         try {
             commentService.save(commentDTO);
+            List<CommentDTO> commentDTOList = commentService.findByBoardId(commentDTO.getBoardId());
+            return new ResponseEntity(commentDTOList, HttpStatus.OK);
+        } catch (NoSuchElementException e){
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity delete(@RequestBody CommentDTO commentDTO){
+        try {
+            commentService.deleteById(commentDTO.getId());
             List<CommentDTO> commentDTOList = commentService.findByBoardId(commentDTO.getBoardId());
             return new ResponseEntity(commentDTOList, HttpStatus.OK);
         } catch (NoSuchElementException e){
--- src/main/java/com/icia/memberboard/service/CommentService.java
+++ src/main/java/com/icia/memberboard/service/CommentService.java
@@ -40,4 +40,8 @@ public class CommentService {
         });
         return commentDTOList;
     }
+
+    public void deleteById(Long id) {
+        commentRepository.deleteById(id);
+    }
 }
